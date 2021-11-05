c5c8c4482405348ebc76bc0a157d8ca5026c3a98
gkraser <gkraser@gmail.com>
2021-11-05 07:24:24 +0600

    ~~

---
+++ docs/src/web/_inc/ext_resource/Project1.groovy
@@ -0,0 +1,25 @@
+package web._inc.ext_resource
+
+import jandcode.core.web.jc.*
+import jandcode.jc.*
+
+class Project1 extends ProjectScript {
+
+    void onInclude() throws Exception {
+
+        // .....
+
+        include(WebRootProject).with {
+            // подключаем каталог ./frontend/_gen к проекту
+            // в виртуальную папку "app", т.е. все в каталоге ./frontend/_gen
+            // будет доступно в приложении по адресу http://HOST/app
+            addResourceWebroot("./frontend/_gen", "app")
+
+            // подключаем каталог ./frontend/public к проекту
+            // в корневую виртуальную папку, т.е. все в каталоге ./frontend/public
+            // будет доступно в приложении по адресу http://HOST/
+            addResourceWebroot("./frontend/public")
+        }
+
+    }
+}
