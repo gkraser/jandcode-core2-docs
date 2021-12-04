58009413ecc9b4809f972677f1a59f4e0699cc57
gkraser <gkraser@gmail.com>
2021-12-04 09:30:51 +0600

    ~~

--- docs/src/db/_inc/dao/DaoSimple1.java
+++ docs/src/db/_inc/dao/DaoSimple1.java
@@ -4,18 +4,12 @@ import jandcode.core.dao.*;
 
 public class DaoSimple1 extends BaseDao {
 
-    @DaoMethod
     public String meth1(String param1) throws Exception {
         return "meth1 PARAM1=" + param1;
     }
 
-    @DaoMethod
     public String meth2(String param1) throws Exception {
         return "meth2 PARAM1=" + param1;
-    }
-
-    // это не dao-метод, его никак не вызвать из вне
-    public void noDaoMethod1() {
     }
 
     // это метод для внутреннего использования внутри dao
