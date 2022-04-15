bbef26f8782deb6e74d09132f65c5cb9e89ef756
gkraser <gkraser@gmail.com>
2022-04-15 16:51:55 +0600

    ~~

---
+++ samples/jandcode-samples-dbm1/jandcode-samples-dbm1-main/src/jandcode/samples/dbm1/main/fixture/Tab1_fixture.groovy
@@ -0,0 +1,23 @@
+package jandcode.samples.dbm1.main.fixture
+
+import jandcode.commons.rnd.*
+import jandcode.core.dbm.fixture.*
+
+/**
+ * Построить тестовые данные для таблицы tab1
+ */
+class Tab1_fixture extends BaseFixtureBuilder {
+
+    Rnd rnd = new Rnd(123)
+
+    int count = 5
+
+    protected void onBuild() {
+        def tab = fx.table("tab1")
+
+        for (id in 1..count) {
+            tab.add(id: id, f_string: "str-${id}", f_int: id * 10)
+        }
+    }
+
+}
